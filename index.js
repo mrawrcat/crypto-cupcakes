625e1ce7f02087ebdc25b15f61d44b70349f6c85
danwen <danwen@penguin>
2023-01-26 16:04:01 -0500

    added use and get /me

--- index.js
+++ index.js
@@ -7,6 +7,8 @@ const { PORT = 3000 } = process.env;
 // TODO - require express-openid-connect and destructure auth from it
 const { auth } = require('express-openid-connect');
 const { User, Cupcake } = require('./db');
+const jwt = require('jsonwebtoken');
+const { JWT_SECRET } = process.env; 
 
 // middleware
 app.use(cors());
@@ -38,15 +40,65 @@ app.use(express.urlencoded({extended:true}));
 
   // auth router attaches /login, /logout, and /callback routes to the baseURL
   app.use(auth(config));
+
+  app.use(async (req, res, next) => {
+    try{
+      const [user] = await User.findOrCreate({
+        where: {
+          username : `${req.oidc.user.nickname}`,
+          name : `${req.oidc.user.name}`,
+          email : `${req.oidc.user.email}`
+        }
+      });
+      console.log('user: ', req.oidc.user)
+      console.log('username: ', req.oidc.user.username)
+      console.log(user)
+      next();
+    }
+    catch(error){
+      next(error);
+    }
+  });
   // req.isAuthenticated is provided from the auth router
   app.get('/', (req, res) => {
-    console.log(req.oidc.user);
-    res.send(req.oidc.isAuthenticated() ? 'Logged in' : 'Logged out');
+    res.send(req.oidc.isAuthenticated() ? `
+    <h2 style="text-align: center;">My Crypto Cupcakes, Inc.</h2>
+    <h2>Welcome, ${req.oidc.user.name}</h2>
+    <p><b>Username: ${req.oidc.user.email}</b></p>
+    <p><b>Email: ${req.oidc.user.email}</p>
+    <img src="${req.oidc.user.picture}" alt="${req.oidc.user.name}">
+    ` : 'Logged out');
   });
+
+  app.get("/me", async(req,res,next)=> {
+    try{
+      // Find user with User.findOne
+      const user = await User.findOne({
+        where: {
+          username: req.oidc.user.nickname
+        },
+        raw: true,
+      });
+  
+      // If/else statement - Assign token with user. No user, no token
+      if(user){
+        const token = jwt.sign(user, JWT_SECRET, { expiresIn: '1w' });
+  
+        // Send back the object {user, token}
+        res.send({user, token})
+  
+      }else{
+        res.status(401).send("No user");
+      }
+    }catch(error){
+      console.error(error);
+      next(error);
+    }
+  })
+  
   
 app.get('/cupcakes', async (req, res, next) => {
   try {
-    //console.log(req.oidc.user);
     const cupcakes = await Cupcake.findAll();
     res.send(cupcakes);
   } catch (error) {
